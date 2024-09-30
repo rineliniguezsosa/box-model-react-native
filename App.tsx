600483cd4178b3179347cbc6d8e4e2e922ed24dd
Rinel <rineliniguezsosa@gmail.com>
2024-09-30 14:19:59 -0600

    [FEAT] Cambios en el App.tsx
    - importando DimensionScreen
    - estableciendo el componente DimensionsScreen en el retorno del component App

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import { BoxObjectModel } from './src/presentation/screen/BoxObjectModel'; 
+// import { BoxObjectModel } from './src/presentation/screen/BoxObjectModel';
+import { DimensionScreen } from './src/presentation/screen/DimensionScreen';
 
 export const App = () => {
   return (
     <SafeAreaView style={{flex:1}}>
-      <BoxObjectModel/>
+      <DimensionScreen/>
     </SafeAreaView>
   );
 };
