171000874294b939188192eda0504161609fef32
Rinel <rineliniguezsosa@gmail.com>
2024-09-30 17:33:19 -0600

    [FEAT] Creando los estilos de clase en el archivo PositionScreen
    - importando StyleSheet
    - creando el objeto styles con la clase container, css prop flex:1 y color de fondo en verde

--- src/presentation/screen/PositionScreen.tsx
+++ src/presentation/screen/PositionScreen.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { View,Text } from 'react-native';
+import { View,Text, StyleSheet } from 'react-native';
 
 export const PositionScreen = () => {
   return (
-    <View>
+    <View style={styles.container}>
         <Text>PositionScreen</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'green',
+    },
+});
