968e9ec74a1d0a2704662fd4015d31ac1bfe49d5
Rinel <rineliniguezsosa@gmail.com>
2024-09-30 13:22:44 -0600

    [FEAT] Creando el funcional component BoxObjectModel

---
+++ src/presentation/screen/BoxObjectModel.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+export const BoxObjectModel = () => {
+  return (
+    <View>
+        <Text>BoxObjectModel</Text>
+    </View>
+  );
+};
