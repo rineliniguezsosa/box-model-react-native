1ff1b24258febd9d659e62bf8049aec0417f2569
Rinel <rineliniguezsosa@gmail.com>
2024-09-30 14:17:14 -0600

    [FEAT] Creando los estilos en el archivo Dimensiones
    - importando el StyleShhet
    -  creando la variable styles pars crear los estilos, creando la clase container con las props css  width:'100%',backgroundColor:'red',
    - estableciendo la clase container en el component principal View

--- src/presentation/screen/DimensionScreen.tsx
+++ src/presentation/screen/DimensionScreen.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { View,Text } from 'react-native';
+import { View,Text,StyleSheet } from 'react-native';
 
 export const DimensionScreen = () => {
   return (
-    <View>
+    <View style={styles.container}>
         <Text>DimensionScreen</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+    container:{
+        width:'100%',
+        backgroundColor:'red',
+    },
+});
