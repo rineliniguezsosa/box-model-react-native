08aa5191ab7b69985bd016cce91c76b672320e2c
Rinel <rineliniguezsosa@gmail.com>
2024-09-30 17:28:51 -0600

    [FEAT] Creando el funcional component PositionScreen

---
+++ src/presentation/screen/PositionScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+export const PositionScreen = () => {
+  return (
+    <View>
+        <Text>PositionScreen</Text>
+    </View>
+  );
+};
