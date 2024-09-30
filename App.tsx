cce97cd421fe31d1acf02637254f763995293ed5
Rinel <rineliniguezsosa@gmail.com>
2024-09-30 17:30:32 -0600

    [FEAT] Importando en el archivo de entrada principal App el componente PositionScreen

--- App.tsx
+++ App.tsx
@@ -2,12 +2,13 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 // import { BoxObjectModel } from './src/presentation/screen/BoxObjectModel';
-import { DimensionScreen } from './src/presentation/screen/DimensionScreen';
+// import { DimensionScreen } from './src/presentation/screen/DimensionScreen';
+import { PositionScreen } from './src/presentation/screen/PositionScreen';
 
 export const App = () => {
   return (
     <SafeAreaView style={{flex:1}}>
-      <DimensionScreen/>
+      <PositionScreen/>
     </SafeAreaView>
   );
 };
