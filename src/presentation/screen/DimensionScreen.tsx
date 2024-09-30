fe08880048e44a6618c5bb0f83cbd7178d35095e
Rinel <rineliniguezsosa@gmail.com>
2024-09-30 14:01:56 -0600

    [FEAT] Estableciendo el funcional component DimensionScreen

---
+++ src/presentation/screen/DimensionScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+export const DimensionScreen = () => {
+  return (
+    <View>
+        <Text>DimensionScreen</Text>
+    </View>
+  );
+};
