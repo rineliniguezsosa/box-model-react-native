d157d82664fc0444c316151d3452ca9702aa2f5a
Rinel <rineliniguezsosa@gmail.com>
2024-09-30 18:37:30 -0600

    [FEAT] Importando el componente FlexScreen en el archivo App, devolviendo el componente FlexScreen

--- App.tsx
+++ App.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 import { SafeAreaView } from 'react-native';
 // import { BoxObjectModel } from './src/presentation/screen/BoxObjectModel';
 // import { DimensionScreen } from './src/presentation/screen/DimensionScreen';
-import { PositionScreen } from './src/presentation/screen/PositionScreen';
+// import { PositionScreen } from './src/presentation/screen/PositionScreen';
+import { FlexScreen } from './src/presentation/screen/FlexScreen';
 
 export const App = () => {
   return (
     <SafeAreaView style={{flex:1}}>
-      <PositionScreen/>
+      <FlexScreen/>
     </SafeAreaView>
   );
 };
