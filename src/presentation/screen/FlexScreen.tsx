5415fa32f1a0b948c9dfc3a2ed992862083519db
Rinel <rineliniguezsosa@gmail.com>
2024-09-30 18:41:33 -0600

    [FEAT] Creando los estilos en el archivo FlexScreen
    - importando StyleSheet
    - creando la variable styles donde estableceremos los estilos css
    - creamos la clase container y las clases box1 al box3
    - estableciendo la clase container al elemento View

--- src/presentation/screen/FlexScreen.tsx
+++ src/presentation/screen/FlexScreen.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text,StyleSheet } from 'react-native';
 
 export const FlexScreen = () => {
   return (
-    <View>
+    <View style={styles.container}>
         <Text>FlexScreen</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'yellow',
+    },
+    box1:{
+        backgroundColor:'red',
+    },
+    box2:{
+        backgroundColor:'green',
+    },
+    box3:{
+        backgroundColor:'blue',
+    },
+});
+
