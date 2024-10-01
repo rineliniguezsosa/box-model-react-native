68fbc4a83197fb7e844eb07b85a0147d40e4c564
Rinel <rineliniguezsosa@gmail.com>
2024-09-30 18:35:44 -0600

    [FEAT] Creando el funcional component FlexScreen

---
+++ src/presentation/screen/FlexScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const FlexScreen = () => {
+  return (
+    <View>
+        <Text>FlexScreen</Text>
+    </View>
+  );
+};
