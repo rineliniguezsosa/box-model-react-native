22dcbdba963081598d78247c01ad98351d8b9422
Rinel <rineliniguezsosa@gmail.com>
2024-09-30 18:51:50 -0600

    [FEAT] Creamos una copia del archivo FlexScreen y lo renombramos por el archivo FlexDirectionScreen

---
+++ src/presentation/screen/FlexDirectionScreen.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View,StyleSheet } from 'react-native';
+
+export const FlexDirectionScreen = () => {
+  return (
+    <View style={styles.container}>
+        <View style={styles.box1} />
+        <View style={styles.box2} />
+        <View style={styles.box3} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'yellow',
+    },
+    box1:{
+        backgroundColor:'red',
+        flex:1,
+    },
+    box2:{
+        backgroundColor:'green',
+        flex:2,
+    },
+    box3:{
+        backgroundColor:'blue',
+        flex:3,
+    },
+});
+
